fefa3a4fee398fb905daca08866161ce12257729
Daniel North <dnorth2@hotmail.com>
2015-02-16 11:35:19 -0700

    Fixed the time complexity by using an O(1) contains function. Added comments to code

--- visualIntelligentScissors/DijkstraScissors.cs
+++ visualIntelligentScissors/DijkstraScissors.cs
@@ -7,10 +7,18 @@ using Priority_Queue;
 
 namespace VisualIntelligentScissors
 {
-	public class DijkstraScissors : Scissors
-	{
+    /*
+     * My HeapPriorityQueue is a high speed priority queue provided by https://bitbucket.org/BlueRaja/high-speed-priority-queue-for-c/wiki/
+     */
+    public class DijkstraScissors : Scissors
+    {
+        //This is a blue colored pen named "yellowpen". You're just going to have to deal with it...
         Pen yellowpen = new Pen(Color.Blue);
 
+        //Node class for the Priority Queue Node. Required by the HeapPriorityQueue documentation
+        //point- The pixel we are at
+        //weight- The weight of the pixel from the previous pixel
+        //previous- The previous pixel from where it came
         public class Node : PriorityQueueNode
         {
             public Point point { get; set; }
@@ -25,13 +33,13 @@ namespace VisualIntelligentScissors
         }
 
 
-		public DijkstraScissors() { }
+        public DijkstraScissors() { }
         /// <summary>
         /// constructor for intelligent scissors. 
         /// </summary>
         /// <param name="image">the image you are oging to segment.  has methods for getting gradient information</param>
         /// <param name="overlay">an overlay on which you can draw stuff by setting pixels.</param>
-		public DijkstraScissors(GrayBitmap image, Bitmap overlay) : base(image, overlay) { }
+        public DijkstraScissors(GrayBitmap image, Bitmap overlay) : base(image, overlay) { }
 
         // this is the class you need to implement in CS 312
 
@@ -40,71 +48,80 @@ namespace VisualIntelligentScissors
         /// </summary>
         /// <param name="points">these are the segmentation points from the pgm file.</param>
         /// <param name="pen">this is a pen you can use to draw on the overlay</param>
-		public override void FindSegmentation(IList<Point> points, Pen pen)
-		{
-			if (Image == null) throw new InvalidOperationException("Set Image property first.");
+        public override void FindSegmentation(IList<Point> points, Pen pen)
+        {
+            if (Image == null) throw new InvalidOperationException("Set Image property first.");
             // this is the entry point for this class when the button is clicked
-            // to do the image segmentation with intelligent scissors.
 
+            //Create a Priority Queue the size of the height and width to account for every pixel
+            HeapPriorityQueue<Node> priorityQueue = new HeapPriorityQueue<Node>(Overlay.Width * Overlay.Height);
+            
+            //Color the Starting Points that are given
             ColorStartingPoints(points);
 
+            //For every point, find the path to that point. points[(i + 1) % points.Count] is to make sure we wrap around to the first point
             for (int i = 0; i < points.Count; i++)
             {
-                PathToPoints(points[i], points[(i + 1) % points.Count]);
+                PathToPoints(priorityQueue, points[i], points[(i + 1) % points.Count]);
             }
-		}
+        }
 
-        private void PathToPoints(Point start, Point end)
+        //start- the starting point
+        //end- the point to end at
+        private void PathToPoints(HeapPriorityQueue<Node> priorityQueue, Point start, Point end)
         {
-            HeapPriorityQueue<Node> priorityQueue = new HeapPriorityQueue<Node>(Overlay.Width * Overlay.Height);
-            List<Point> queue = new List<Point>();
+            HashSet<Point> queue = new HashSet<Point>();
 
             //Queue the starting node. Its weight is 0
+            //Because the priority Queue .Contains() matches based on exact nodes, and the information within the node could be different (although we could be at the same position)
+            //I found it easier to add the point to a HashSet and remove it when the node is removed from the PriorityQueue. That way I can search the contents of the HashSet in O(1) time
+            //.Add(), .Remove(), and .Contains() is all in O(1) time in a HashSet, meaning it doesn't add any complexity besides a constant
             priorityQueue.Enqueue(new Node(start, 0, null), 0);
+            queue.Add(start);
             Boolean foundGoal = false;
-            List<Point> settledSet = new List<Point>();
+            HashSet<Point> settledSet = new HashSet<Point>();
             Node finalNode = null;
 
+            //While we haven't found the goal and the priorityQueue isn't empty
             while (priorityQueue.Count != 0 && !foundGoal)
             {
+                //Take the top priority node off the queue
                 Node priorityNode = priorityQueue.Dequeue();
+                queue.Remove(priorityNode.point);
+
+                //Add that node to the settled set
                 settledSet.Add(priorityNode.point);
 
+                //Get the neighbors of that pixel
                 List<Node> neighbors = GetNeighborNodes(priorityNode);
 
-                foreach(Node neighbor in neighbors)
+                foreach (Node neighbor in neighbors)
                 {
-                    //Console.WriteLine("Does the settledSet have me: {0} Does the priorityQueue have me: {1}", settledSet.Contains(neighbor.point), pQueueContains(priorityQueue, neighbor));
-                    if(neighbor.point == end)
+                    //If the neighbor is equal to the end point, we've found our goal and we want to exit and remember that neighbor.
+                    if (neighbor.point == end)
                     {
                         finalNode = neighbor;
                         foundGoal = true;
                     }
-                    else if (!settledSet.Contains(neighbor.point) && !pQueueContains(priorityQueue, neighbor))
+                    else if (!settledSet.Contains(neighbor.point) && !queue.Contains(neighbor.point))
                     {
+                        //If the settled set doesn't contain the neighbor and the queue doesn't contain it either, let's add it to the queue and make its priority the weight
                         priorityQueue.Enqueue(neighbor, neighbor.weight);
-                        //Console.WriteLine("Adding- X: {0} Y: {1} Weight: {2}", neighbor.point.X, neighbor.point.Y, neighbor.weight);
+                        queue.Add(neighbor.point);
                     }
                 }
             }
 
-            PrintNodeTrace(finalNode);
+            //Clear the queue, draw the points based on the trace of previous nodes until you get to null, and start on the next point
+            priorityQueue.Clear();
+            DrawNodeTrace(finalNode);
         }
 
-        private bool pQueueContains(HeapPriorityQueue<Node> priorityQueue, Node compared)
-        {
-            int count = 0;
-            foreach(Node node in priorityQueue)
-            {
-                if(node.point == compared.point)
-                {
-                    return true;
-                }
-                count++;
-            }
-            return false;
-        }
-
+        //Each node can have 4 neighbors- N, S, E, W
+        //Create the neighbors and add them to the list as long as they're within the picture
+        //We add N, E, S, W in order in case of clockwise weight collisions
+        //priorityNode- the node we are going to find neighbors on
+        //returns: A list of neighbor nodes
         private List<Node> GetNeighborNodes(Node priorityNode)
         {
             List<Node> neighbors = new List<Node>();
@@ -114,7 +131,7 @@ namespace VisualIntelligentScissors
             Node sNode = new Node(new Point(priorityNode.point.X, priorityNode.point.Y + 1), this.GetPixelWeight(new Point(priorityNode.point.X, priorityNode.point.Y + 1)) + priorityNode.weight, priorityNode);
             Node wNode = new Node(new Point(priorityNode.point.X - 1, priorityNode.point.Y), this.GetPixelWeight(new Point(priorityNode.point.X - 1, priorityNode.point.Y)) + priorityNode.weight, priorityNode);
 
-            if(WithinPicture(nNode.point))
+            if (WithinPicture(nNode.point))
             {
                 neighbors.Add(nNode);
             }
@@ -134,11 +151,15 @@ namespace VisualIntelligentScissors
             return neighbors;
         }
 
+        //point- the point to check whether or not it's in the image
+        //returns: a boolean based on whether or not the point is within the image
         private Boolean WithinPicture(Point point)
         {
             return (point.X < (Overlay.Width - 2) && point.Y < (Overlay.Height - 2) && point.X > 1 && point.Y > 1);
         }
 
+        //points- The list of starting points
+        //Draws a circle around the starting points for convenience
         private void ColorStartingPoints(IList<Point> points)
         {
             using (Graphics g = Graphics.FromImage(Overlay))
@@ -152,13 +173,15 @@ namespace VisualIntelligentScissors
             }
         }
 
-        private void PrintNodeTrace(Node finalNode)
+        //finalNode- the last node in a trace of nodes that lead back to the original 
+        //Until the previous node is null, recursively draw pixels on the current node
+        private void DrawNodeTrace(Node finalNode)
         {
             Overlay.SetPixel(finalNode.point.X, finalNode.point.Y, Color.Red);
-            if(finalNode.previous != null)
+            if (finalNode.previous != null)
             {
-                PrintNodeTrace(finalNode.previous);
+                DrawNodeTrace(finalNode.previous);
             }
         }
-	}
+    }
 }
--- visualIntelligentScissors/SimpleScissors.cs
+++ visualIntelligentScissors/SimpleScissors.cs
@@ -33,14 +33,18 @@ namespace VisualIntelligentScissors
             
 			if (Image == null) throw new InvalidOperationException("Set Image property first.");
 
+            //Color the Starting Points that are given
             ColorStartingPoints(points);
 
+            //For every point, find the path to that point. points[(i + 1) % points.Count] is to make sure we wrap around to the first point
             for(int i = 0; i < points.Count; i++)
             {
                 PathToPoints(points[i], points[(i + 1) % points.Count]);
             }
 		}
 
+        //points- The list of starting points
+        //Draws a circle around the starting points for convenience
         private void ColorStartingPoints(IList<Point> points)
         {
             using (Graphics g = Graphics.FromImage(Overlay))
@@ -54,19 +58,25 @@ namespace VisualIntelligentScissors
             }
         }
 
+        //start- the starting point
+        //end- the point to end at
         private void PathToPoints(Point start, Point end)
         {
-            List<Point> visited = new List<Point>();
+            //Create a new HashSet to not visit nodes that are already visited
+            HashSet<Point> visited = new HashSet<Point>();
             Point currPoint = start;
 
+            //While the current point is not equal to the end point
             while(currPoint != end)
             {
+                //Draw the current pixel and add it to the visited list. Also, set an arbitrarily large integer as a weight
                 Overlay.SetPixel(currPoint.X, currPoint.Y, Color.Red);
                 visited.Add(currPoint);
 
                 int leastPointWeight = int.MaxValue;
 
-                //The lower Y value is more north
+                //Find all neighbor points and weights N, E, S, W
+                //The lower Y value is more north because of the image setup
                 Point nPoint = new Point(currPoint.X, currPoint.Y - 1);
                 Point ePoint = new Point(currPoint.X + 1, currPoint.Y);
                 Point sPoint = new Point(currPoint.X, currPoint.Y + 1);
@@ -77,6 +87,8 @@ namespace VisualIntelligentScissors
                 int sWeight = this.GetPixelWeight(sPoint);
                 int wWeight = this.GetPixelWeight(wPoint);
 
+                //Get the next point based on whether or not the point is within the picture, the point has already been visited, and its weight is less than the previous point
+                //Because of the way this is setup, it will go in a clockwise fashion- N, E, S, W and will only accept smaller weights once a weight is accepted
                 if(WithinPicture(nPoint) && !visited.Contains(nPoint) && nWeight < leastPointWeight)
                 {
                     currPoint = nPoint;
@@ -101,6 +113,7 @@ namespace VisualIntelligentScissors
                     leastPointWeight = wWeight;
                 }
 
+                //If our weight never changed, meaning we didn't go N, E, S, W then we've reached a dead end. Return
                 if (leastPointWeight == int.MaxValue)
                 {
                     break;
@@ -108,6 +121,8 @@ namespace VisualIntelligentScissors
             }
         }
 
+        //finalNode- the last node in a trace of nodes that lead back to the original 
+        //Until the previous node is null, recursively draw pixels on the current node
         private Boolean WithinPicture(Point point)
         {
             return (point.X < (Overlay.Width - 2) && point.Y < (Overlay.Height - 2) && point.X > 1 && point.Y > 1);  
